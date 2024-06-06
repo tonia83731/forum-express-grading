cc23cc9ed5606145e683168b996c704ffdac33e6
huiyu.tonia <huiyu.tonia.chang@gmail.com>
2024-06-06 08:40:01 +0800

    feat: add index page

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const restaurantController = require("../controllers/restaurant-controller");
 
-module.exports = router
+router.get("/restaurants", restaurantController.getRestaurants);
+
+router.use("/", (req, res) => res.redirect("/restaurants"));
+module.exports = router;
