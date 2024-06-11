ae9cc91d05b37321a90abe525abc5dc88f13f992
huiyu.tonia <huiyu.tonia.chang@gmail.com>
2024-06-11 11:29:37 +0800

    feat: add restaurant model

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,7 +1,13 @@
 const restaurantController = {
   getRestaurants: (req, res) => {
-    return res.render('restaurants')
-  }
-}
+    return res.render("restaurants");
+  },
+  getRestaurant: async (req, res, next) => {},
+  createRestaurant: async (req, res, next) => {},
+  postRestaurant: async (req, res, next) => {},
+  editRestaurant: async (req, res, next) => {},
+  putRestaurant: async (req, res, next) => {},
+  deleteRestaurant: async (req, res, next) => {},
+};
 
-module.exports = restaurantController
+module.exports = restaurantController;
