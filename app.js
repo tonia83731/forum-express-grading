52d64a62060374b9bdb1353722d691cfbd3b6934
huiyu.tonia <huiyu.tonia.chang@gmail.com>
2024-06-11 17:04:30 +0800

    feat: admin restaurant

--- app.js
+++ app.js
@@ -1,41 +1,43 @@
-const express = require('express')
-const handlebars = require('express-handlebars')
-const flash = require('connect-flash')
-const session = require('express-session')
-const passport = require('./config/passport')
-const routes = require('./routes')
-const handlebarsHelpers = require('./helpers/handlebars-helpers')
-const { getUser } = require('./helpers/auth-helpers')
+const express = require("express");
+const handlebars = require("express-handlebars");
+const flash = require("connect-flash");
+const methodOverride = require("method-override");
+const session = require("express-session");
+const passport = require("./config/passport");
+const routes = require("./routes");
+const handlebarsHelpers = require("./helpers/handlebars-helpers");
+const { getUser } = require("./helpers/auth-helpers");
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 // const db = require("./models");
-const SESSION_SECRET = 'secret'
+const SESSION_SECRET = "secret";
 // 註冊 Handlebars 樣板引擎，並指定副檔名為 .hbs
-app.engine('hbs', handlebars({ extname: '.hbs', helpers: handlebarsHelpers }))
+app.engine("hbs", handlebars({ extname: ".hbs", helpers: handlebarsHelpers }));
 // 設定使用 Handlebars 做為樣板引擎
-app.set('view engine', 'hbs')
-app.use(express.urlencoded({ extended: true }))
+app.set("view engine", "hbs");
+app.use(express.urlencoded({ extended: true }));
 app.use(
   session({
     secret: SESSION_SECRET,
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
   })
-)
-app.use(passport.initialize())
-app.use(passport.session())
-app.use(flash())
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+app.use(methodOverride("_method"));
 app.use((req, res, next) => {
-  res.locals.success_messages = req.flash('success_messages')
-  res.locals.error_messages = req.flash('error_messages')
-  res.locals.user = getUser(req)
-  next()
-})
-app.use(routes)
+  res.locals.success_messages = req.flash("success_messages");
+  res.locals.error_messages = req.flash("error_messages");
+  res.locals.user = getUser(req);
+  next();
+});
+app.use(routes);
 
 app.listen(port, () => {
-  console.info(`Example app listening on http://localhost:${port}/`)
-})
+  console.info(`Example app listening on http://localhost:${port}/`);
+});
 
-module.exports = app
+module.exports = app;
