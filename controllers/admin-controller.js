4f4ae2680023a84bfc0fa8422cfa20d6147bb228
huiyu.tonia <huiyu.tonia.chang@gmail.com>
2024-06-11 11:33:13 +0800

    feat: modify admin restaurants page

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,6 +1,22 @@
+const { Restaurant } = require("../models");
+
 const adminController = {
-  getRestaurants: (req, res) => {
-    return res.render('admin/restaurants')
-  }
-}
-module.exports = adminController
+  getRestaurants: async (req, res, next) => {
+    // return res.render("admin/restaurants");
+    try {
+      const restaurants = await Restaurant.findAll({
+        raw: true,
+      });
+      res.render("admin/restaurants", { restaurants });
+    } catch (error) {
+      next(error);
+    }
+  },
+  getRestaurant: async (req, res, next) => {},
+  createRestaurant: async (req, res, next) => {},
+  postRestaurant: async (req, res, next) => {},
+  editRestaurant: async (req, res, next) => {},
+  putRestaurant: async (req, res, next) => {},
+  deleteRestaurant: async (req, res, next) => {},
+};
+module.exports = adminController;
