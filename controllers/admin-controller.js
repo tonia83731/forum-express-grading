9722ba29ad587b228796d3fb42249ae3859e7640
huiyu.tonia <huiyu.tonia.chang@gmail.com>
2024-06-13 08:34:31 +0800

    feat: add admin user page

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,95 +1,119 @@
-const { Restaurant } = require('../models')
-const { localFileHandler } = require('../helpers/file-helpers')
+const { Restaurant, User } = require("../models");
+const { localFileHandler } = require("../helpers/file-helpers");
 
 const adminController = {
   getRestaurants: async (req, res, next) => {
     // return res.render("admin/restaurants");
     try {
       const restaurants = await Restaurant.findAll({
-        raw: true
-      })
-      res.render('admin/restaurants', { restaurants })
+        raw: true,
+      });
+      res.render("admin/restaurants", { restaurants });
     } catch (error) {
-      next(error)
+      next(error);
     }
   },
   getRestaurant: async (req, res, next) => {
     try {
-      const id = req.params.id
-      const restaurant = await Restaurant.findByPk(id, { raw: true })
-      if (!restaurant) throw new Error("Restaurant didn't exist!")
-      res.render('admin/restaurant', { restaurant })
+      const id = req.params.id;
+      const restaurant = await Restaurant.findByPk(id, { raw: true });
+      if (!restaurant) throw new Error("Restaurant didn't exist!");
+      res.render("admin/restaurant", { restaurant });
     } catch (error) {
-      next(error)
+      next(error);
     }
   },
   createRestaurant: (req, res) => {
-    return res.render('admin/create-restaurant')
+    return res.render("admin/create-restaurant");
   },
   postRestaurant: async (req, res, next) => {
     try {
-      const { name, tel, address, openingHours, description } = req.body
-      if (!name) throw new Error('Restaurant name is required!')
-      const { file } = req
-      const filepath = await localFileHandler(file)
+      const { name, tel, address, openingHours, description } = req.body;
+      if (!name) throw new Error("Restaurant name is required!");
+      const { file } = req;
+      const filepath = await localFileHandler(file);
       await Restaurant.create({
         name,
         tel,
         address,
         openingHours,
         description,
-        image: filepath || null
-      })
-      req.flash('success_messages', 'restaurant was successfully created')
-      res.redirect('/admin/restaurants')
+        image: filepath || null,
+      });
+      req.flash("success_messages", "restaurant was successfully created");
+      res.redirect("/admin/restaurants");
     } catch (error) {
-      next(error)
+      next(error);
     }
   },
   editRestaurant: async (req, res, next) => {
     try {
-      const id = req.params.id
-      const restaurant = await Restaurant.findByPk(id, { raw: true })
-      if (!restaurant) throw new Error("Restaurant didn't exist!")
-      res.render('admin/edit-restaurant', { restaurant })
+      const id = req.params.id;
+      const restaurant = await Restaurant.findByPk(id, { raw: true });
+      if (!restaurant) throw new Error("Restaurant didn't exist!");
+      res.render("admin/edit-restaurant", { restaurant });
     } catch (error) {
-      next(error)
+      next(error);
     }
   },
   putRestaurant: async (req, res, next) => {
     try {
-      const { name, tel, address, openingHours, description } = req.body
-      const id = req.params.id
-      if (!name) throw new Error('Restaurant name is required!')
-      const { file } = req
+      const { name, tel, address, openingHours, description } = req.body;
+      const id = req.params.id;
+      if (!name) throw new Error("Restaurant name is required!");
+      const { file } = req;
       const [restaurant, filepath] = await Promise.all([
         Restaurant.findByPk(id),
-        localFileHandler(file)
-      ])
+        localFileHandler(file),
+      ]);
       await restaurant.update({
         name,
         tel,
         address,
         openingHours,
         description,
-        image: filepath || restaurant.image
-      })
-      req.flash('success_messages', 'restaurant was successfully to update')
-      res.redirect('/admin/restaurants')
+        image: filepath || restaurant.image,
+      });
+      req.flash("success_messages", "restaurant was successfully to update");
+      res.redirect("/admin/restaurants");
     } catch (error) {
-      next(error)
+      next(error);
     }
   },
   deleteRestaurant: async (req, res, next) => {
     try {
-      const id = req.params.id
-      const restaurant = await Restaurant.findByPk(id)
-      if (!restaurant) throw new Error("Restaurant didn't exist!")
-      await restaurant.destroy()
-      res.redirect('/admin/restaurants')
+      const id = req.params.id;
+      const restaurant = await Restaurant.findByPk(id);
+      if (!restaurant) throw new Error("Restaurant didn't exist!");
+      await restaurant.destroy();
+      res.redirect("/admin/restaurants");
     } catch (error) {
-      next(error)
+      next(error);
     }
-  }
-}
-module.exports = adminController
+  },
+  getUser: async (req, res, next) => {
+    try {
+      const users = await User.findAll({
+        raw: true,
+      });
+      console.log(users);
+      res.render("admin/users", { users });
+    } catch (error) {
+      next(error);
+    }
+  },
+  patchUser: async (req, res, next) => {
+    try {
+      const id = req.params.id;
+      const user = await User.findByPk(id);
+      if (!user) throw new Error("User didn't exist!");
+      await user.update({
+        isAdmin: !user.isAdmin,
+      });
+      res.redirect("/admin/users");
+    } catch (error) {
+      next(error);
+    }
+  },
+};
+module.exports = adminController;
