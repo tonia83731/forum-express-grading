6dad03786a850a17a6239d8f936f3cbb319a943c
huiyu.tonia <huiyu.tonia.chang@gmail.com>
2024-06-17 15:34:54 +0800

    feat: add restaurants index page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,13 +1,41 @@
+const { Restaurant, Category } = require("../models");
 const restaurantController = {
-  getRestaurants: (req, res) => {
-    return res.render('restaurants')
+  getRestaurants: async (req, res, next) => {
+    // return res.render('restaurants')
+    try {
+      const restaurants = await Restaurant.findAll({
+        raw: true,
+        nest: true,
+        include: Category,
+      });
+      const data = restaurants.map((r) => {
+        return {
+          ...r,
+          description: r.description.substring(0, 50),
+        };
+      });
+      res.render("restaurants", { restaurants: data });
+    } catch (error) {
+      next(error);
+    }
   },
-  getRestaurant: async (req, res, next) => {},
+  getRestaurant: async (req, res, next) => {
+    try {
+      const id = res.params.id;
+      const restaurant = await Restaurant.findByPk(id, {
+        raw: true,
+        nest: true,
+        include: Category,
+      });
+    } catch (error) {
+      next(error);
+    }
+  },
   createRestaurant: async (req, res, next) => {},
   postRestaurant: async (req, res, next) => {},
   editRestaurant: async (req, res, next) => {},
   putRestaurant: async (req, res, next) => {},
-  deleteRestaurant: async (req, res, next) => {}
-}
+  deleteRestaurant: async (req, res, next) => {},
+};
 
-module.exports = restaurantController
+module.exports = restaurantController;
