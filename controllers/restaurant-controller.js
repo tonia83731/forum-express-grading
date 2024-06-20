9b2034cb63a39efb4725340a8c9cf7af65233e32
huiyu.tonia <huiyu.tonia.chang@gmail.com>
2024-06-20 16:42:42 +0800

    feat: add comment count to dashboard

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,49 +1,49 @@
-const { Restaurant, Category, Comment, User } = require('../models')
-const { getOffset, getPagination } = require('../helpers/pagination-helper')
+const { Restaurant, Category, Comment, User } = require("../models");
+const { getOffset, getPagination } = require("../helpers/pagination-helper");
 const restaurantController = {
   getRestaurants: async (req, res, next) => {
     // return res.render('restaurants')
     try {
-      const DEFAULT_LIMIT = 9
-      const categoryId = Number(req.query.categoryId) || ''
-      const page = Number(req.query.page) || 1
-      const limit = Number(req.query.limit) || DEFAULT_LIMIT
-      const offset = getOffset(limit, page)
+      const DEFAULT_LIMIT = 9;
+      const categoryId = Number(req.query.categoryId) || "";
+      const page = Number(req.query.page) || 1;
+      const limit = Number(req.query.limit) || DEFAULT_LIMIT;
+      const offset = getOffset(limit, page);
       const [restaurants, categories] = await Promise.all([
         Restaurant.findAndCountAll({
           raw: true,
           nest: true,
           include: Category,
           where: {
-            ...(categoryId ? { categoryId } : {})
+            ...(categoryId ? { categoryId } : {}),
           },
           limit,
-          offset
+          offset,
         }),
         Category.findAll({
-          raw: true
-        })
-      ])
+          raw: true,
+        }),
+      ]);
 
-      const data = restaurants.rows.map(r => {
+      const data = restaurants.rows.map((r) => {
         return {
           ...r,
-          description: r.description.substring(0, 50)
-        }
-      })
-      res.render('restaurants', {
+          description: r.description.substring(0, 50),
+        };
+      });
+      res.render("restaurants", {
         restaurants: data,
         categories,
         categoryId,
-        pagination: getPagination(limit, page, restaurants.count)
-      })
+        pagination: getPagination(limit, page, restaurants.count),
+      });
     } catch (error) {
-      next(error)
+      next(error);
     }
   },
   getRestaurant: async (req, res, next) => {
     try {
-      const id = req.params.id
+      const id = req.params.id;
       const restaurant = await Restaurant.findByPk(id, {
         // raw: true,
         nest: true,
@@ -51,33 +51,49 @@ const restaurantController = {
           Category,
           {
             model: Comment,
-            include: User
-          }
-        ]
-      })
-      if (!restaurant) throw new Error("Restaurant didn't exist!")
-      await restaurant.increment('viewCounts')
+            include: User,
+          },
+        ],
+      });
+      if (!restaurant) throw new Error("Restaurant didn't exist!");
+      await restaurant.increment("viewCounts");
       // console.log(increment);
-      res.render('restaurant', { restaurant: restaurant.toJSON() })
+      res.render("restaurant", { restaurant: restaurant.toJSON() });
     } catch (error) {
-      next(error)
+      next(error);
     }
   },
   getDashboard: async (req, res, next) => {
     // return res.render("dashboard");
     try {
-      const id = req.params.id
-      const restaurant = await Restaurant.findByPk(id, {
-        raw: true,
-        nest: true,
-        include: Category
-      })
-      if (!restaurant) throw new Error("Restaurant didn't exist!")
-      return res.render('dashboard', { viewCounts: restaurant.viewCounts })
+      const id = req.params.id;
+      const [restaurant, comments] = await Promise.all([
+        Restaurant.findByPk(id, {
+          raw: true,
+          nest: true,
+          include: Category,
+        }),
+        Comment.findAndCountAll({
+          nest: true,
+          where: { restaurantId: id },
+        }),
+      ]);
+      // console.log(comments);
+      const commentCounts = comments.count;
+      // const restaurant = await Restaurant.findByPk(id, {
+      //   raw: true,
+      //   nest: true,
+      //   include: Category
+      // })
+      if (!restaurant) throw new Error("Restaurant didn't exist!");
+      return res.render("dashboard", {
+        viewCounts: restaurant.viewCounts,
+        commentCounts,
+      });
     } catch (error) {
-      next(error)
+      next(error);
     }
-  }
-}
+  },
+};
 
-module.exports = restaurantController
+module.exports = restaurantController;
