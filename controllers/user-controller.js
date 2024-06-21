1357a70b4f02161907acba5c4e3a5d7c7e2bcf0f
huiyu.tonia <huiyu.tonia.chang@gmail.com>
2024-06-21 17:45:57 +0800

    feat: add favorite/unfavorite button at index page

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,112 +1,151 @@
-const bcrypt = require('bcryptjs')
-const db = require('../models')
-const { User, Comment, Restaurant } = db
+const bcrypt = require("bcryptjs");
+const db = require("../models");
+const { User, Comment, Restaurant, Favorite } = db;
 const {
   // localFileHandler,
-  imgurFileHandler
-} = require('../helpers/file-helpers')
+  imgurFileHandler,
+} = require("../helpers/file-helpers");
 const userController = {
   signUpPage: (req, res) => {
-    res.render('signup')
+    res.render("signup");
   },
   signUp: async (req, res, next) => {
     try {
-      const { name, email, password, passwordCheck } = req.body
+      const { name, email, password, passwordCheck } = req.body;
       if (password !== passwordCheck) {
-        throw new Error('Passwords do not match!')
+        throw new Error("Passwords do not match!");
       }
 
       const user = await User.findOne({
-        where: { email }
-      })
-      if (user) throw new Error('Email already exists!')
-      const hash = await bcrypt.hash(password, 10)
+        where: { email },
+      });
+      if (user) throw new Error("Email already exists!");
+      const hash = await bcrypt.hash(password, 10);
       await User.create({
         name,
         email,
-        password: hash
-      })
-      req.flash('success_messages', '成功註冊帳號！')
-      res.redirect('/signin')
+        password: hash,
+      });
+      req.flash("success_messages", "成功註冊帳號！");
+      res.redirect("/signin");
     } catch (error) {
-      next(error)
+      next(error);
     }
   },
   signInPage: (req, res) => {
-    res.render('signin')
+    res.render("signin");
   },
   signIn: (req, res) => {
-    req.flash('success_messages', '成功登入!')
-    if (req.user.isAdmin) return res.redirect('/admin/restaurants')
-    res.redirect('/restaurants')
+    req.flash("success_messages", "成功登入!");
+    if (req.user.isAdmin) return res.redirect("/admin/restaurants");
+    res.redirect("/restaurants");
   },
   logout: (req, res) => {
-    req.flash('success_messages', '成功登出!')
-    req.logout()
-    res.redirect('/signin')
+    req.flash("success_messages", "成功登出!");
+    req.logout();
+    res.redirect("/signin");
   },
   getUser: async (req, res, next) => {
     try {
-      const id = req.params.id
+      const id = req.params.id;
       const [user, comments] = await Promise.all([
         User.findByPk(id, {
-          raw: true
+          raw: true,
         }),
         Comment.findAndCountAll({
           nest: true,
           // raw: true,
           where: { userId: id },
-          include: Restaurant
-        })
-      ])
-      if (!user) throw new Error("User didn't exist!")
+          include: Restaurant,
+        }),
+      ]);
+      if (!user) throw new Error("User didn't exist!");
       // console.log(comments);
       const restData = comments.rows
-        .map(item => item.Restaurant.toJSON())
+        .map((item) => item.Restaurant.toJSON())
         .reduce((acc, restaurant) => {
-          acc.set(restaurant.id, restaurant)
-          return acc
-        }, new Map())
-      const restaurants = Array.from(restData.values())
+          acc.set(restaurant.id, restaurant);
+          return acc;
+        }, new Map());
+      const restaurants = Array.from(restData.values());
       // console.log(restaurants);
-      const commentCounts = comments.count
-      const restCounts = restaurants.length
-      res.render('profile', { user, restaurants, commentCounts, restCounts })
+      const commentCounts = comments.count;
+      const restCounts = restaurants.length;
+      res.render("profile", { user, restaurants, commentCounts, restCounts });
     } catch (error) {
-      next(error)
+      next(error);
     }
   },
   editUser: async (req, res, next) => {
     try {
-      const id = req.params.id
-      const user = await User.findByPk(id, { raw: true })
-      if (!user) throw new Error("User didn't exist!")
-      res.render('edit-profile', { user })
+      const id = req.params.id;
+      const user = await User.findByPk(id, { raw: true });
+      if (!user) throw new Error("User didn't exist!");
+      res.render("edit-profile", { user });
     } catch (error) {
-      next(error)
+      next(error);
     }
   },
   putUser: async (req, res, next) => {
     try {
-      const id = req.params.id
-      const name = req.body.name
-      console.log(name)
-      if (!name) throw new Error('User name is required!')
-      const { file } = req
+      const id = req.params.id;
+      const name = req.body.name;
+      console.log(name);
+      if (!name) throw new Error("User name is required!");
+      const { file } = req;
       const [user, filePath] = await Promise.all([
         User.findByPk(id),
-        imgurFileHandler(file)
-      ])
+        imgurFileHandler(file),
+      ]);
       await user.update({
         name,
-        image: filePath || user.image
-      })
-      req.flash('success_messages', 'user was successfully to update')
-      res.redirect(`/users/${id}`)
+        image: filePath || user.image,
+      });
+      req.flash("success_messages", "user was successfully to update");
+      res.redirect(`/users/${id}`);
     } catch (error) {
-      next(error)
+      next(error);
     }
-  }
-}
+  },
+  addFavorite: async (req, res, next) => {
+    try {
+      const { restaurantId } = req.params;
+      const [restaurant, favorite] = await Promise.all([
+        Restaurant.findByPk(restaurantId),
+        Favorite.findOne({
+          where: {
+            userId: req.user.id,
+            restaurantId,
+          },
+        }),
+      ]);
+      if (!restaurant) throw new Error("Restaurant didn't exist!");
+      if (favorite) throw new Error("You have favorited this restaurant!");
+      await Favorite.create({
+        userId: req.user.id,
+        restaurantId,
+      });
+      res.redirect("back");
+    } catch (error) {
+      next(error);
+    }
+  },
+  removeFavorite: async (req, res, next) => {
+    try {
+      const { restaurantId } = req.params;
+      const favorite = await Favorite.findOne({
+        where: {
+          userId: req.user.id,
+          restaurantId,
+        },
+      });
+      if (!favorite) throw new Error("You haven't favorited this restaurant");
+      favorite.destroy();
+      res.redirect("back");
+    } catch (error) {
+      next(error);
+    }
+  },
+};
 
-module.exports = userController
+module.exports = userController;
