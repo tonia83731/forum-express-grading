ec996721088748b5c0d73caf4389c77a5357253a
huiyu.tonia <huiyu.tonia.chang@gmail.com>
2024-06-21 17:58:11 +0800

    feat: get user's favorited restaruants & switch button

--- config/passport.js
+++ config/passport.js
@@ -1,49 +1,57 @@
-const passport = require('passport')
-const LocalStrategy = require('passport-local')
-const bcrypt = require('bcryptjs')
-const db = require('../models')
-const User = db.User
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const bcrypt = require("bcryptjs");
+// const db = require('../models')
+// const User = db.User
+const { User, Restaurant } = require("../models");
 
 passport.use(
   new LocalStrategy(
     // customize user field
     {
-      usernameField: 'email',
-      passwordField: 'password',
-      passReqToCallback: true
+      usernameField: "email",
+      passwordField: "password",
+      passReqToCallback: true,
     },
     // authenticate user
     (req, email, password, cb) => {
-      User.findOne({ where: { email } }).then(user => {
+      User.findOne({ where: { email } }).then((user) => {
         if (!user) {
           return cb(
             null,
             false,
-            req.flash('error_messages', '帳號或密碼輸入錯誤！')
-          )
+            req.flash("error_messages", "帳號或密碼輸入錯誤！")
+          );
         }
-        bcrypt.compare(password, user.password).then(res => {
+        bcrypt.compare(password, user.password).then((res) => {
           if (!res) {
             return cb(
               null,
               false,
-              req.flash('error_messages', '帳號或密碼輸入錯誤！')
-            )
+              req.flash("error_messages", "帳號或密碼輸入錯誤！")
+            );
           }
-          return cb(null, user)
-        })
-      })
+          return cb(null, user);
+        });
+      });
     }
   )
-)
+);
 passport.serializeUser((user, cb) => {
-  cb(null, user.id)
-})
+  cb(null, user.id);
+});
 passport.deserializeUser(async (id, cb) => {
-  let user = await User.findByPk(id)
-  user = user.toJSON()
+  let user = await User.findByPk(id, {
+    include: [
+      {
+        model: Restaurant,
+        as: "FavoritedRestaurants",
+      },
+    ],
+  });
+  user = user.toJSON();
   // console.log(user)
-  return cb(null, user)
-})
+  return cb(null, user);
+});
 
-module.exports = passport
+module.exports = passport;
--- routes/index.js
+++ routes/index.js
@@ -1,59 +1,59 @@
-const express = require("express");
-const router = express.Router();
-const passport = require("../config/passport");
-const restaurantController = require("../controllers/restaurant-controller");
-const userController = require("../controllers/user-controller");
-const commentController = require("../controllers/comment-controller");
-const { authenticated, authenticatedAdmin } = require("../middleware/auth");
-const { generalErrorHandler } = require("../middleware/error-handler");
-const admin = require("./modules/admin");
-const upload = require("../middleware/multer");
-router.use("/admin", authenticatedAdmin, admin);
+const express = require('express')
+const router = express.Router()
+const passport = require('../config/passport')
+const restaurantController = require('../controllers/restaurant-controller')
+const userController = require('../controllers/user-controller')
+const commentController = require('../controllers/comment-controller')
+const { authenticated, authenticatedAdmin } = require('../middleware/auth')
+const { generalErrorHandler } = require('../middleware/error-handler')
+const admin = require('./modules/admin')
+const upload = require('../middleware/multer')
+router.use('/admin', authenticatedAdmin, admin)
 // signup, signin, logout
-router.get("/signup", userController.signUpPage);
-router.post("/signup", userController.signUp);
-router.get("/signin", userController.signInPage);
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
+router.get('/signin', userController.signInPage)
 router.post(
-  "/signin",
-  passport.authenticate("local", {
-    failureRedirect: "/signin",
-    failureFlash: true,
+  '/signin',
+  passport.authenticate('local', {
+    failureRedirect: '/signin',
+    failureFlash: true
   }),
   userController.signIn
-); // 注意是 post
-router.get("/logout", userController.logout);
-router.get("/restaurants/feeds", authenticated, restaurantController.getFeeds);
+) // 注意是 post
+router.get('/logout', userController.logout)
+router.get('/restaurants/feeds', authenticated, restaurantController.getFeeds)
 router.get(
-  "/restaurants/:id/dashboard",
+  '/restaurants/:id/dashboard',
   authenticated,
   restaurantController.getDashboard
-);
+)
 router.get(
-  "/restaurants/:id",
+  '/restaurants/:id',
   authenticated,
   restaurantController.getRestaurant
-);
-router.get("/users/:id", userController.getUser);
-router.get("/users/:id/edit", userController.editUser);
-router.put("/users/:id", upload.single("image"), userController.putUser);
+)
+router.get('/users/:id', userController.getUser)
+router.get('/users/:id/edit', userController.editUser)
+router.put('/users/:id', upload.single('image'), userController.putUser)
 router.delete(
-  "/comments/:id",
+  '/comments/:id',
   authenticatedAdmin,
   commentController.deleteComment
-);
-router.post("/comments", authenticated, commentController.postComment);
+)
+router.post('/comments', authenticated, commentController.postComment)
 router.post(
-  "/favorite/:restaurantId",
+  '/favorite/:restaurantId',
   authenticated,
   userController.addFavorite
-);
+)
 router.delete(
-  "/favorite/:restaurantId",
+  '/favorite/:restaurantId',
   authenticated,
   userController.removeFavorite
-);
+)
 // restaurants
-router.get("/restaurants", authenticated, restaurantController.getRestaurants);
-router.use("/", (req, res) => res.redirect("/restaurants"));
-router.use("/", generalErrorHandler);
-module.exports = router;
+router.get('/restaurants', authenticated, restaurantController.getRestaurants)
+router.use('/', (req, res) => res.redirect('/restaurants'))
+router.use('/', generalErrorHandler)
+module.exports = router
